4a25aca7eba4fda42c8bf5f0e15f91ac7f3196cc
kjss <982356589@qq.com>
2021-03-31 10:45:18 +0800

    big_news_server 项目结构搭建完毕

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')        //cors跨域设置
+
+const app = express()
+
+
+
+
+
+
+
+app.listen(3000, () => { console.log('big_news_server start 3000!!!!!'); }) //服务器开启 端口3000
